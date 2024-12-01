9f1739548c3b3c0f7fd8f3be9a78d101e97bb41b
Jakub Latoch <172971794+Rezzum@users.noreply.github.com>
2024-12-01 19:47:27 +0100

    Solve the task acc to the exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  const tempCelsius = ((tempFahrenheit - 32) * (5/9));
+  let roundedTempCelsius = 0;
+
+  if (Number.isInteger(tempCelsius) === false) {
+    roundedTempCelsius = parseFloat(tempCelsius.toFixed(1));
+    return roundedTempCelsius;
+  }
+  return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  const tempFahrenheit = ((tempCelsius * (9/5)) + 32);
+  let roundedTempFahrenheit = 0;
+
+  if(Number.isInteger(tempFahrenheit) === false) {
+    roundedTempFahrenheit = parseFloat(tempFahrenheit.toFixed(1));
+    return roundedTempFahrenheit;
+  }
+  return tempFahrenheit;
 };
 
 // Do not edit below this line
