e782cda1f2eb86d4e3364bcc87d1880cf514fd55
Jakub Latoch <172971794+Rezzum@users.noreply.github.com>
2024-12-01 11:10:54 +0100

    Solve the task acc to the exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let returnString = "";
+    let splitString = string.split("");
 
+    for (let i = splitString.length - 1; i >= 0; i--) {
+        returnString += splitString[i];
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
