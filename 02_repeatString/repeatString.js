edc4dabfd08328eab22a2ac1ee430c9a049fde48
Jakub Latoch <172971794+Rezzum@users.noreply.github.com>
2024-12-01 10:18:54 +0100

    Solve the task acc to the exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let printedString = '';
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        printedString += string;
+    }
+    return printedString;
 };
 
 // Do not edit below this line
