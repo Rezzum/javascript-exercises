ee329dc9d2ce3a55fbd5d91caf608f63b69aacc0
Jakub Latoch <172971794+Rezzum@users.noreply.github.com>
2024-12-01 17:55:36 +0100

    Solve the task acc to the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    
+    if (isNaN(num1) || isNaN(num2) || num1 < 0 || num2 < 0 ||
+        Number.isInteger(num1) === false || Number.isInteger(num2) === false) {
+        return "ERROR";
+    };
 
+    if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = num2; i <= num1; i++) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
