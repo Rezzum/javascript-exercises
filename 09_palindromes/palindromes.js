0caef333494c0cb46f78410577e022011bfb48ed
Jakub Latoch <172971794+Rezzum@users.noreply.github.com>
2024-12-16 14:08:15 +0100

    Solve acc to instructions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const filteredArray = Array.from(string.toLowerCase()).filter((char) => (char.match(/^[a-z0-9]+$/i)));
+    const backwardsArray = filteredArray.slice().reverse();
+    if (filteredArray.join("") === backwardsArray.join("")) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+palindromes("r3ace3car");
 
 // Do not edit below this line
 module.exports = palindromes;
