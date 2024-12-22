2ee161da1dfcf713023a4683df6b03e90889b07f
Jakub Latoch <172971794+Rezzum@users.noreply.github.com>
2024-12-22 17:44:34 +0100

    Solve acc to instructions

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
 
+    array.forEach(person => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = currentYear;
+        }
+    });
+
+    const oldestPerson = array.sort(
+        (personA, personB) => (personB.yearOfDeath - personB.yearOfBirth) - 
+        (personA.yearOfDeath - personA.yearOfBirth));
+    return oldestPerson[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
