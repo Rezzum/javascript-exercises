9d56c739de9b8c03e4714e4b71a433150d4c2a71
Jakub Latoch <172971794+Rezzum@users.noreply.github.com>
2024-12-22 15:19:49 +0100

    Solve acc to instructions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let val = parseInt(number);
+    let fib = [1, 1];
 
+    if (val < 0) {
+        return "OOPS";
+    } else if (val === 0) {
+        return 0;
+    } else {
+        for (let i = 2; i < val; i++) {
+            fib.push(fib[i-1] + fib[i-2]);
+        }
+        return fib[val-1];
+    } 
 };
 
 // Do not edit below this line
